a2220736b0ef70c8b3e0dda5099164c900b30906
Kate3009 <dayanavalderrama.2909@gmail.com>
2024-09-18 22:25:14 -0500

    cambios

---
+++ src/main/java/com/prestamo/kafka/entity/CatalogoCreateEvent.java
@@ -0,0 +1,9 @@
+package com.prestamo.kafka.entity;
+
+import com.prestamo.entity.Catalogo;
+import com.prestamo.entity.Pais;
+import com.prestamo.kafka.config.Event;
+
+public class CatalogoCreateEvent extends Event<Catalogo>{
+
+}
